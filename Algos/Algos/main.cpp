1cd1883bbea4c7b98bfbd9f2b7471faf743ba985
Sudhanshu-Gupta <sudhanshu.gupta05@gmail.com>
2019-08-06 10:50:17 +0530

    algos

---
+++ Algos/Algos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algos
+//
+//  Created by Sudhanshu Gupta on 06/08/19.
+//  Copyright © 2019 Sudhanshu Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
